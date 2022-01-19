e47ee1ce70b7aa48ba4a1323ae3634cba55acf62
zalfanuramalia <zalfafanur@gmail.com>
2022-01-19 21:12:10 +0700

    revisi 1 tugas javascript lanjutan

--- nilai.js
+++ nilai.js
@@ -1,9 +1,14 @@
-const selNilai = (5, 20)
-const arr = [2, 25, 4, 14, 17, 30, 8]
-
-
-if (awal < akhir){
-    if (arr > 5){
-        
+function SeleksiNilai (awal, akhir, arr){
+    if (awal < akhir){
+        if (arr.length > 5){
+            console.log(arr.filter(el => el > 5 && el < 20))
+        } 
+        else {
+            console.log("Jumlah angka dalam dataArray harus lebih dari 5")
+        }
+    } else {
+        console.log("Nilai akhir harus lebih besar dari nilai awal")
     }
 }
+
+SeleksiNilai(4,17,[2,25,4])
