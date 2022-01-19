6fee3a8487b587205163510e36f2da00d83a1fb9
zalfanuramalia <zalfafanur@gmail.com>
2022-01-19 22:47:46 +0700

    file tugas javascript lanjutan

--- nilai.js
+++ nilai.js
@@ -1,14 +1,18 @@
 function SeleksiNilai (awal, akhir, arr){
-    if (awal < akhir){
+    if (awal < akhir && awal !== akhir){
         if (arr.length > 5){
-            console.log(arr.filter(el => el > 5 && el < 20))
+            const res = arr.filter(el => el > 5 && el < 20);
+            res.sort(function (a,b){
+                return a - b
+            });
+            console.log(res);
         } 
         else {
-            console.log("Jumlah angka dalam dataArray harus lebih dari 5")
+            console.log("Jumlah angka dalam dataArray harus lebih dari 5") 
         }
     } else {
         console.log("Nilai akhir harus lebih besar dari nilai awal")
     }
 }
 
-SeleksiNilai(4,17,[2,25,4])
+SeleksiNilai(5,  20, [2, 25, 4, 14, 17, 30, 8])
--- search.js
+++ search.js
@@ -1,14 +1,28 @@
-const nama = [
-    "Abigail", "Alexandra", "Alison","Amanda", "Angela", "Bella","Carol","Caroline", 
-    "Carolyn","Deirdre", "Diana", "Elizabeth","Ella", "Faith", "Olivia", "Penelope"];
-
-function find(nama, cb) {
-    cb = cb.split(""); 
-    return nama.filter(function(word) { 
-      return cb.every(function(char) {
-        return word.includes(char); 
-        });
-    });
+const name = [
+  "Abigail", "Alexandra", "Alison",
+  "Amanda", "Angela", "Bella",
+  "Carol", "Caroline", "Carolyn",
+  "Deirdre", "Diana", "Elizabeth",
+  "Ella", "Faith", "Olivia", "Penelope"]
+  
+  function searchname(sentence, num, callback)
+  {
+    const tempnamefilter = name.filter(word => {
+      return word.toLowerCase().includes(sentence)
+    })
+    if(tempnamefilter !== 0)
+    {
+      return tempnamefilter.slice(0, num)
+    }
+    else
+    {
+      console.log("Kata tidak Cocok!")
+    }
+    callback(tempnamefilter) 
   }
   
-  console.log(find(nama, "al"));
+  const showname = (lists) => {
+    console.log("ini callback")
+    lists.map(list => console.log(lists))
+  }
+  console.log(searchname("al", 3, showname))
